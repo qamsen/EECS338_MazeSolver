57ee98fd6a654132d29724b973191852e600d1f6
krd42@case.edu <krd42@case.edu>
2018-04-17 14:13:10 -0400

    Finished implementing the Maze class.

--- src/Maze.java
+++ src/Maze.java
@@ -4,14 +4,9 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Set;
 
-class Maze {
+public class Maze {
 
-    private final Room start;
-    private final Room end;
-    private final Dimension mazeSize;
-    private final Set<Room> rooms;
-
-    static class Room {
+    public static class Room {
 
         private final Point location;
         private final Set<Room> adjacentRooms;
@@ -21,15 +16,17 @@ class Maze {
             this.adjacentRooms = new HashSet<Room>();
         }
 
-        Set<Room> getAdjacentRooms() {
-            return adjacentRooms;
-        }
-
+        /**
+         * @return the location of the room in the maze
+         */
         public Point getLocation() {
             return location;
         }
+        private Set<Room> getAdjacentRooms() {
+            return adjacentRooms;
+        }
 
-        void addAdjacentRoom(Room adjacentRoom) {
+        private void addConnection(Room adjacentRoom) {
             adjacentRooms.add(adjacentRoom);
         }
 
@@ -60,15 +57,76 @@ class Maze {
 
     }
 
-    private Maze(Room start, Room end, Dimension mazeSize, Set<Room> rooms) {
+    private final Room start;
+    private final Room end;
+    private final Dimension mazeSize;
+    private final HashMap<Room, Room> rooms;
+
+    private Maze(Room start, Room end, Dimension mazeSize, HashMap<Room, Room> rooms) {
         this.start = start;
         this.end = end;
         this.mazeSize = mazeSize;
         this.rooms = rooms;
     }
 
-    // TODO: Implement the Maze builder
-    static Maze buildMaze(boolean[][] maze, Point startPoint, Point endPoint) {
+    /**
+     * @return the start room of the maze
+     */
+    public Room getStart() {
+        return start;
+    }
+
+    /**
+     * @return the end room of the maze
+     */
+    public Room getEnd() {
+        return end;
+    }
+
+    /**
+     * @return the dimensions of the maze
+     */
+    public Dimension getMazeSize() {
+        return mazeSize;
+    }
+
+    /**
+     * @return the set of all rooms in this maze
+     */
+    public Set<Room> getRooms() {
+        return new HashSet<Room>(rooms.values());
+    }
+
+    /**
+     * @param room  a given room in the maze
+     * @return      the set of all rooms adjacent to the given room
+     */
+    public Set<Room> adjacentRooms(Room room) {
+
+        Set<Room> adjacentRooms = new HashSet<Room>();
+
+        // Maintains the connections in the Maze
+        for (Room adjacentRoom : room.getAdjacentRooms()) {
+            adjacentRooms.add(rooms.get(adjacentRoom));
+        }
+
+        return adjacentRooms;
+    }
+
+    /**
+     * Builds a maze given a 2d boolean matrix, where true implies a room and
+     * false implies a wall.
+     *
+     * @param maze        matrix that represents the maze
+     * @param startPoint  start point of the maze
+     * @param endPoint    end point of the maze
+     * @return            the maze as a graph
+     */
+    static Maze buildMaze(
+            boolean[][] maze,
+            Point startPoint,
+            Point endPoint
+            ) {
 
         // Preconditions: non-empty maze, start & end point in the maze
         assert maze != null;
@@ -85,22 +143,53 @@ class Maze {
             for (int j = 0; j < maze[0].length; j++) {
 
                 // True when there exists a room at i, j
-                if (maze[i][j]) {
-                }
+                if (maze[i][j])
+                    buildRoom(maze, new Point(i, j), rooms);
             }
         }
 
-        return null;
+        start = buildRoom(maze, startPoint, rooms);
+        end = buildRoom(maze, endPoint, rooms);
+
+        return new Maze(start, end, mazeSize, rooms);
     }
 
-    private static Room buildRoom(boolean[][] maze, Point point, HashMap<Room, Room> rooms) {
+    private static Room buildRoom(
+            boolean[][] maze,
+            Point roomPoint,
+            HashMap<Room, Room> rooms
+            ) {
 
-        assert isIn(maze, point);
+        assert isIn(maze, roomPoint);
 
-        return null;
+        Room room = new Room(roomPoint);
+
+        if (rooms.containsKey(room))
+            return rooms.get(room);
+
+        for (Point adjacentPoint : adjacentPoints(roomPoint)) {
+
+            if (isRoom(maze, adjacentPoint)) {
+                Room adjacentRoom = new Room(adjacentPoint);
+                room.addConnection(adjacentRoom);
+            }
+        }
+
+        rooms.put(room, room);
+        return room;
     }
 
-    static private boolean isIn(boolean[][] maze, Point point) {
+    private static boolean isRoom(boolean[][] maze, Point point) {
+
+        if (isIn(maze, point)) {
+            return maze[point.x][point.y];
+        } else {
+            return false;
+        }
+
+    }
+
+    private static boolean isIn(boolean[][] maze, Point point) {
 
         boolean inRangeX = 0 <= point.x && point.x < maze.length;
         boolean inRangeY = 0 <= point.y && point.y < maze[0].length;
@@ -108,7 +197,7 @@ class Maze {
         return inRangeX && inRangeY;
     }
 
-    Set<Point> adjacentPoints(Point point) {
+    private static Set<Point> adjacentPoints(Point point) {
 
         Set<Point> adjacentPoints = new HashSet<Point>();
 
