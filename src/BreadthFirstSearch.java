02202e284bc878ca11133b482058a421c89bf8e0
cbonnell28 <cbonnell28@gmail.com>
2018-04-23 03:06:54 -0400

    Adding BFS method

---
+++ src/BreadthFirstSearch.java
@@ -0,0 +1,16 @@
+
+public class BreadthFirstSearch {
+	
+	//private Set<Room> visited;
+	
+	public static Maze runBFS(Maze maze) {
+		
+		
+		return maze;
+	}
+	
+	public static void main(String args[]) {
+		System.out.print("BFS");
+	}
+	
+}
